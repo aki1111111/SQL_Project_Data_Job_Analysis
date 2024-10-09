835c87287298c59ccbee9571004fcd6bdf791998
Aki <slashrealeza19@gmail.com>
2024-10-09 21:00:31 +0800

    WITH top_paying_jobs AS ( SELECT job_id, job_work_from_home, job_schedule_type, name AS Company_name, salary_year_avg From     job_postings_fact AS jpf LEFT JOIN     company_dim AS cd ON     cd.company_id = jpf.company_id  WHERE      salary_year_avg IS NOT NULL AND      job_title_short = 'Data Analyst' AND     job_work_from_home = true  Order by     salary_year_avg DESC
    Limit 10
    )
    
    SELECT
    tpj.*,
    skills
    FROM top_paying_jobs AS tpj
    INNER JOIN skills_job_dim  AS sjd
    ON tpj.job_id = sjd.job_id
    INNER JOIN skills_dim as sd
    ON sjd.skill_id = sd.skill_id

--- project_sql/2_Top_paying_roles.sql
+++ project_sql/2_Top_paying_roles.sql
@@ -1,15 +1,32 @@
-SELECT * FROM job_postings_fact;
-SELECT * FROM skills_job_dim
-limit 1000; --2nd db
-SELECT * FROM skills_dim -- 3rd db
 
+WITH top_paying_jobs AS (
+SELECT 
+job_id,
+job_work_from_home,
+job_schedule_type,
+name AS Company_name,
+salary_year_avg
+From 
+    job_postings_fact AS jpf
+LEFT JOIN 
+    company_dim AS cd
+ON
+    cd.company_id = jpf.company_id
+ WHERE 
+     salary_year_avg IS NOT NULL AND 
+     job_title_short = 'Data Analyst' AND 
+    job_work_from_home = true
+ Order by 
+    salary_year_avg DESC
+ 
+Limit 10
+)
 
 SELECT 
-sjd.skill_id,
-count(*) as skill_count
-FROM skills_job_dim As sjd
-INNER JOIN job_postings_fact AS jpf
-ON jpf.job_id = sjd.job_id
-GROUP BY
-sjd.skill_id
-Limit 1000;
+tpj.*,
+skills
+FROM top_paying_jobs AS tpj
+INNER JOIN skills_job_dim  AS sjd
+ON tpj.job_id = sjd.job_id
+INNER JOIN skills_dim as sd
+ON sjd.skill_id = sd.skill_id
